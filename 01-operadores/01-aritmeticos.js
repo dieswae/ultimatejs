08ae9ca37a0c618bc6645059455f50df73b821a6
dieswae <diegooare11@gmail.com>
2024-04-08 22:08:02 -0300

    comenzando control de flujo

--- 01-operadores/01-aritmeticos.js
+++
@@ -1,9 +0,0 @@
-let a = 5;
-let b = 7;
-
-console.log(a + b); // adicion
-console.log(a - b); // resta
-console.log(a * b); // multiplicacion
-console.log(a / b); // division
-console.log(a % b); // modulo
-console.log(a ** b); // potencia
---
+++ 03-control-flujo/05-do-while.js
@@ -0,0 +1,17 @@
+// cuales son los numeros pares
+
+let i = 0
+
+while (i < 3) {
+  if (i % 2 == 0) {
+    console.log('numero par', i);
+  }
+  i++
+}
+
+do {
+  if (i % 2 == 0) {
+    console.log('numero par', i);
+  }
+  i++
+} while (i < 3)
